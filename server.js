791efd44387a26a04e9c62ecad4adc7a2f5917e5
chakalisriram <sriramchkali@gmail.com>
2025-03-14 00:53:49 +0530

    Fix deployment issues

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const path = require("path");
-const Post = require("./Post"); // Import Post model
+const Post = require("./models/Post"); // Import Post model
 
 dotenv.config();
 
